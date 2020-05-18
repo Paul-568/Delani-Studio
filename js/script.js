4b17485cfe4bda181d210c8c95f7c23b0ddb606d
Paul <ptuleto@gmail.com>
2020-05-18 09:01:42 +0300

    made pop boxes when mouse hoovers in portfolio page

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ $(document).ready(function(){
         $(this).children('.hideMe').slideToggle(1200);
         $(this).children('.show').toggle(1500);
       });
+$('.card').hover(function(event){
+      $(".card").hover(function () {
+        $(this).children(".card-1").fadeToggle('fast', "linear");
+      });
+      });
+      event.preventDefault();
