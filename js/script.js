a19b5f01095a742d81cb720080f49630366a8ffd
Paul <ptuleto@gmail.com>
2020-05-18 08:58:57 +0300

    hide services,design, and product management using jquerry

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    
+    $('.main').click(function () {
+        $(this).children('.hideMe').slideToggle(1200);
+        $(this).children('.show').toggle(1500);
+      });
