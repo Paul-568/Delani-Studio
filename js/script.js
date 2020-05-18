8809fa58ef125f2c62e11e445afc7f2fa262f3bb
Paul <ptuleto@gmail.com>
2020-05-18 09:03:18 +0300

    alerted user when he enters his name and email

--- js/script.js
+++ js/script.js
@@ -10,3 +10,19 @@ $('.card').hover(function(event){
       });
       });
       event.preventDefault();
+      $('form#myForm').submit(function(){
+      var name = $('#nameDetail').val();
+      var pass = $('#email').val();
+      var mess = $('#mess').val();
+    });
+    $("button").on('click',function(){
+      var client = document.getElementById(nameDetail).value;
+      alert(" Dear " + " , we have received your message and we will get in touch. Thank you for contacting us.");
+    });
+    });
+    
+    $("button").on('click', function(){
+      $('form').each(function(){
+        this.reset();
+      });
+    });
